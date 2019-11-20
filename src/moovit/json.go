0f7228efe4a90092ddc7e083fe651e51542b77f5
Mauro Mastrapasqua <mauro.mastrapasqua@studenti.unimi.it>
2019-11-20 21:39:06 +0100

    Pulizia interface{} da json

--- src/moovit/json.go
+++ src/moovit/json.go
@@ -13,25 +13,19 @@ type InlineSuggestionResult []struct {
 	ID    int `json:"id"`
 	Image struct {
 		ImageID    int         `json:"imageId"`
-		Parameters interface{} `json:"parameters"`
 	} `json:"image"`
 	Title    string `json:"title"`
 	SubTitle []struct {
 		Text  string      `json:"text"`
-		Image interface{} `json:"image"`
 	} `json:"subTitle"`
 	LatLon struct {
 		Latitude  int `json:"latitude"`
 		Longitude int `json:"longitude"`
 	} `json:"latLon"`
-	SortingInfo      interface{} `json:"sortingInfo"`
 	UID              string      `json:"uid"`
-	InaccurateLatLon interface{} `json:"inaccurateLatLon"`
 	GeocoderID       int         `json:"geocoderId"`
 	AirDistance      int         `json:"airDistance"`
 	Source           int         `json:"source"`
-	SourceID         interface{} `json:"sourceId"`
-	SourceDesc       interface{} `json:"sourceDesc"`
 }
 
 type SomeKey struct {
@@ -39,7 +33,6 @@ type SomeKey struct {
 	TilesURL          string      `json:"tilesUrl"`
 	MetroID           string      `json:"metroId"`
 	IsUnresolvedMetro bool        `json:"isUnresolvedMetro"`
-	GtfsLanguage      interface{} `json:"gtfsLanguage"`
 }
 
 type Location struct {
@@ -52,21 +45,16 @@ type LocationResult struct {
 	ID    int `json:"id"`
 	Image struct {
 		ImageID    int         `json:"imageId"`
-		Parameters interface{} `json:"parameters"`
 	} `json:"image"`
 	Title    string `json:"title"`
 	SubTitle []struct {
 		Text  string      `json:"text"`
-		Image interface{} `json:"image"`
 	} `json:"subTitle"`
 	LatLon struct {
 		Latitude  int `json:"latitude"`
 		Longitude int `json:"longitude"`
 	} `json:"latLon"`
-	SortingInfo      interface{} `json:"sortingInfo"`
-	UID              interface{} `json:"uid"`
 	InaccurateLatLon bool        `json:"inaccurateLatLon"`
-	GeocoderID       interface{} `json:"geocoderId"`
 	AirDistance      int         `json:"airDistance"`
 	Source           int         `json:"source"`
 }
@@ -101,21 +89,14 @@ type TripResult struct {
 									Longitude int `json:"longitude"`
 								} `json:"latlon"`
 								Type             int         `json:"type"`
-								UID              interface{} `json:"uid"`
-								InaccurateLatLon interface{} `json:"inaccurateLatLon"`
-								Source           interface{} `json:"source"`
 							} `json:"origin"`
 							Dest struct {
-								Caption interface{} `json:"caption"`
 								ID      int         `json:"id"`
 								Latlon  struct {
 									Latitude  int `json:"latitude"`
 									Longitude int `json:"longitude"`
 								} `json:"latlon"`
 								Type             int         `json:"type"`
-								UID              interface{} `json:"uid"`
-								InaccurateLatLon interface{} `json:"inaccurateLatLon"`
-								Source           interface{} `json:"source"`
 							} `json:"dest"`
 						} `json:"journey"`
 						Shape struct {
@@ -125,42 +106,24 @@ type TripResult struct {
 						WalkingInstructoins []struct {
 							Direction struct {
 								Relative int         `json:"relative"`
-								Absolute interface{} `json:"absolute"`
 							} `json:"direction"`
 							StreetName string `json:"streetName"`
 						} `json:"walkingInstructoins"`
 					} `json:"walkLeg"`
-					WaitToLineLeg           interface{} `json:"waitToLineLeg"`
-					LineLeg                 interface{} `json:"lineLeg"`
-					WaitToTaxiLeg           interface{} `json:"waitToTaxiLeg"`
-					TaxiLeg                 interface{} `json:"taxiLeg"`
-					MultiLineLeg            interface{} `json:"multiLineLeg"`
-					CarpoolRideLeg          interface{} `json:"carpoolRideLeg"`
-					PathwayWalkLeg          interface{} `json:"pathwayWalkLeg"`
-					WaitToMultiLineLeg      interface{} `json:"waitToMultiLineLeg"`
-					LineWithAlternarivesLeg interface{} `json:"lineWithAlternarivesLeg"`
-					BicycleLeg              interface{} `json:"bicycleLeg"`
-					BicycleRentalLeg        interface{} `json:"bicycleRentalLeg"`
-					EventLeg                interface{} `json:"eventLeg"`
-					ParkingLeg              interface{} `json:"parkingLeg"`
 				} `json:"legs"`
 				HasPrev             bool        `json:"hasPrev"`
 				HasNext             bool        `json:"hasNext"`
 				RelevantForRealtime bool        `json:"relevantForRealtime"`
 				IsAccessible        bool        `json:"isAccessible"`
-				ItineraryFare       interface{} `json:"itineraryFare"`
 			} `json:"itinerary"`
 			TripPlanSections struct {
 				TripPlanSections []struct {
 					Name              string      `json:"name"`
 					SectionID         int         `json:"sectionId"`
-					MaxItemsToDisplay interface{} `json:"maxItemsToDisplay"`
 					SectionType       int         `json:"sectionType"`
 				} `json:"tripPlanSections"`
 				IsOtpRt bool `json:"isOtpRt"`
 			} `json:"tripPlanSections"`
-			Advertisment      interface{} `json:"advertisment"`
-			SectionMatchCount interface{} `json:"sectionMatchCount"`
 		} `json:"result"`
 		SupplementalData struct {
 			LineGroupSummaryList []struct {
@@ -176,10 +139,8 @@ type TripResult struct {
 				} `json:"imageRefSet"`
 				LineSummaries []struct {
 					LineID        int         `json:"lineId"`
-					Origin        interface{} `json:"origin"`
 					Destination   string      `json:"destination"`
 					RouteLongName string      `json:"routeLongName"`
-					SubGroupID    interface{} `json:"subGroupId"`
 				} `json:"lineSummaries"`
 				InnerImageIds struct {
 					Images []struct {
@@ -190,8 +151,6 @@ type TripResult struct {
 				Caption1   string      `json:"caption1"`
 				Caption2   string      `json:"caption2"`
 				Type       int         `json:"type"`
-				SubGroups  interface{} `json:"subGroups"`
-				PdfFileURL interface{} `json:"pdfFileUrl"`
 			} `json:"lineGroupSummaryList"`
 			MVStopSyncedMetaDataList []struct {
 				StopID       int    `json:"stopId"`
@@ -203,26 +162,20 @@ type TripResult struct {
 				StopCode      string `json:"stopCode"`
 				Image         int    `json:"image"`
 				PlatformLines []struct {
-					Name    interface{} `json:"name"`
 					LineIds []int       `json:"lineIds"`
 				} `json:"platformLines"`
-				KeyLineIds  []interface{} `json:"keyLineIds"`
-				SitesList   []interface{} `json:"sitesList"`
 				ImageRefSet struct {
 					Images []struct {
 						Index    int `json:"index"`
 						ImageRef struct {
 							ImageID    int         `json:"imageId"`
-							Parameters interface{} `json:"parameters"`
 						} `json:"imageRef"`
 					} `json:"images"`
 				} `json:"imageRefSet"`
-				MvPathways           interface{} `json:"mvPathways"`
 				RouteType            int         `json:"routeType"`
 				WheelchairAccessible bool        `json:"wheelchairAccessible"`
 			} `json:"mVStopSyncedMetaDataList"`
 		} `json:"supplementalData"`
 	} `json:"results"`
-	Errors    interface{} `json:"errors"`
 	Completed bool        `json:"completed"`
 }
--- src/waze/json.go
+++ src/waze/json.go
@@ -12,53 +12,30 @@ type TripResult struct {
 					Direction bool    `json:"direction"`
 				} `json:"path"`
 				Street                   int           `json:"street"`
-				AltStreets               interface{}   `json:"altStreets"`
 				Distance                 int           `json:"distance"`
 				Length                   int           `json:"length"`
 				CrossTime                int           `json:"crossTime"`
 				CrossTimeWithoutRealTime int           `json:"crossTimeWithoutRealTime"`
-				Tiles                    interface{}   `json:"tiles"`
-				ClientIds                interface{}   `json:"clientIds"`
 				KnownDirection           bool          `json:"knownDirection"`
 				Penalty                  int           `json:"penalty"`
 				RoadType                 int           `json:"roadType"`
 				IsToll                   bool          `json:"isToll"`
-				NaiveRoute               interface{}   `json:"naiveRoute"`
 				DetourSavings            int           `json:"detourSavings"`
 				DetourSavingsNoRT        int           `json:"detourSavingsNoRT"`
 				UseHovLane               bool          `json:"useHovLane"`
 				Attributes               int           `json:"attributes"`
 				Lane                     string        `json:"lane"`
-				LaneType                 interface{}   `json:"laneType"`
-				Areas                    []interface{} `json:"areas"`
-				RequiredPermits          []interface{} `json:"requiredPermits"`
-				DetourRoute              interface{}   `json:"detourRoute"`
-				NaiveRouteFullResult     interface{}   `json:"naiveRouteFullResult"`
-				DetourRouteFullResult    interface{}   `json:"detourRouteFullResult"`
 				MergeOffset              int           `json:"mergeOffset"`
 				AvoidStatus              string        `json:"avoidStatus"`
-				ClientLaneSet            interface{}   `json:"clientLaneSet"`
-				AdditionalInstruction    interface{}   `json:"additionalInstruction"`
 				Instruction              struct {
 					Opcode          string      `json:"opcode"`
 					Arg             int         `json:"arg"`
-					InstructionText interface{} `json:"instructionText"`
-					LaneGuidance    interface{} `json:"laneGuidance"`
-					Name            interface{} `json:"name"`
-					Tts             interface{} `json:"tts"`
 				} `json:"instruction"`
 			} `json:"results"`
-			StreetNames             []interface{} `json:"streetNames"`
-			TileIds                 []interface{} `json:"tileIds"`
-			TileUpdateTimes         []interface{} `json:"tileUpdateTimes"`
-			Geom                    interface{}   `json:"geom"`
 			FromFraction            float64       `json:"fromFraction"`
 			ToFraction              float64       `json:"toFraction"`
 			SameFromSegment         bool          `json:"sameFromSegment"`
 			SameToSegment           bool          `json:"sameToSegment"`
-			AstarPoints             interface{}   `json:"astarPoints"`
-			WayPointIndexes         interface{}   `json:"wayPointIndexes"`
-			WayPointFractions       interface{}   `json:"wayPointFractions"`
 			TollMeters              int           `json:"tollMeters"`
 			PreferedRouteID         int           `json:"preferedRouteId"`
 			IsInvalid               bool          `json:"isInvalid"`
@@ -67,30 +44,19 @@ type TripResult struct {
 			DisplayRoute            bool          `json:"displayRoute"`
 			AstarVisited            int           `json:"astarVisited"`
 			AstarResult             string        `json:"astarResult"`
-			AstarData               interface{}   `json:"astarData"`
 			IsRestricted            bool          `json:"isRestricted"`
 			AvoidStatus             string        `json:"avoidStatus"`
-			DueToOverride           interface{}   `json:"dueToOverride"`
 			PassesThroughDangerArea bool          `json:"passesThroughDangerArea"`
 			DistanceFromSource      int           `json:"distanceFromSource"`
 			DistanceFromTarget      int           `json:"distanceFromTarget"`
 			MinPassengers           int           `json:"minPassengers"`
 			HovIndex                int           `json:"hovIndex"`
-			TimeZone                interface{}   `json:"timeZone"`
 			RouteType               []string      `json:"routeType"`
-			RouteAttr               []interface{} `json:"routeAttr"`
 			AstarCost               int           `json:"astarCost"`
 			ReorderChoice           []bool        `json:"reorderChoice"`
 			TotalRouteTime          int           `json:"totalRouteTime"`
-			LaneTypes               []interface{} `json:"laneTypes"`
-			PreferredStoppingPoints interface{}   `json:"preferredStoppingPoints"`
-			Areas                   []interface{} `json:"areas"`
-			RequiredPermits         []interface{} `json:"requiredPermits"`
-			EtaHistograms           []interface{} `json:"etaHistograms"`
-			EntryPoint              interface{}   `json:"entryPoint"`
 			ShortRouteName          string        `json:"shortRouteName"`
 			TollPrice               float64       `json:"tollPrice"`
-			SegGeoms                interface{}   `json:"segGeoms"`
 			RouteName               string        `json:"routeName"`
 			RouteNameStreetIds      []int         `json:"routeNameStreetIds"`
 			Open                    bool          `json:"open"`
@@ -100,6 +66,5 @@ type TripResult struct {
 			Y float64 `json:"y"`
 			Z string  `json:"z"`
 		} `json:"coords"`
-		SegCoords interface{} `json:"segCoords"`
 	} `json:"alternatives"`
 }
