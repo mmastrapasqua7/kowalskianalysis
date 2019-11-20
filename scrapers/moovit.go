6db33e8c782f430771ca59a5dd2033b8d30308c2
Mauro Mastrapasqua <mauro.mastrapasqua@studenti.unimi.it>
2019-11-20 21:13:18 +0100

    Pulizia print di debugging

--- scrapers/moovit.go
+++ scrapers/moovit.go
@@ -21,49 +21,41 @@ type Location struct {
 
 func main() {
 	if len(os.Args) < 5 {
-		fmt.Println("usage:\n\t moovit <startLatitude> <startLongitude> <endLatitude> <endLongitude>")
+		fmt.Println("usage:\n\t moovit <startLat> <startLon> <endLat> <endLon>")
 		return
 	}
 
 	startPoint := Location{}
 	startPoint.Latitude = os.Args[1]
 	startPoint.Longitude = os.Args[2]
-
 	endPoint := Location{}
 	endPoint.Latitude = os.Args[3]
 	endPoint.Longitude = os.Args[4]
 
+	// Scraping sequence
 	// 1
 	moovit.GetWebPage()
-	fmt.Println("1 done")
 
 	// 2
 	startPoint.Name = moovit.GetLocationName(startPoint.Latitude, startPoint.Longitude)
 	endPoint.Name = moovit.GetLocationName(endPoint.Latitude, endPoint.Longitude)
-	fmt.Println("2 done")
 
 	// 3
 	headerParams := moovit.GetParamsNeededForHeader(startPoint.Name, endPoint.Name)
-	fmt.Println("3 done")
 
 	// 4
 	cookie := moovit.GetMagicCookie(headerParams)
-	fmt.Println("4 done")
 
 	// 5
-	_ = moovit.GetMagicKey(headerParams, cookie) // not needed
-	fmt.Println("5 done")
+	_ = moovit.GetMagicKey(headerParams, cookie) // not needed (?)
 
 	// 6
 	startPointMetadata := moovit.GetLocationInfo(startPoint.Name, headerParams, cookie)
 	endPointMetadata := moovit.GetLocationInfo(endPoint.Name, headerParams, cookie)
-	fmt.Println("6 done")
 
 	// 7
 	token := moovit.GetMagicToken(startPointMetadata, endPointMetadata, headerParams, cookie)
-	fmt.Println("7 done")
 
 	// 8
 	moovit.PrintTripPlans(startPointMetadata, endPointMetadata, token, cookie)
-	fmt.Println("8 done")
 }
--- scrapers/waze.go
+++ scrapers/waze.go
@@ -1,3 +1,6 @@
+// Date 2 coordinate (latitudine, longitudine) di partenza e arrivo,
+// lo scraper chiede a Waze di calcolare in tempo reale il miglior percorso
+// in automobile per percorrere la tratta
 package main
 
 import (
@@ -12,16 +15,22 @@ func main() {
 		fmt.Println("usage:\n\t waze <startLat> <startLon> <endLat> <endLon>")
 		return
 	}
+
 	startLat := os.Args[1]
 	startLon := os.Args[2]
 	endLat := os.Args[3]
 	endLon := os.Args[4]
 
+	// Scraping sequence
+	// 1
 	waze.GetWebPage()
 
+	// 2
 	cookies := waze.GetCookies()
 
+	// 3
 	waze.SetCookieConsent()
 
+	// 4
 	waze.GetTripPlans(startLat, startLon, endLat, endLon, cookies)
 }
