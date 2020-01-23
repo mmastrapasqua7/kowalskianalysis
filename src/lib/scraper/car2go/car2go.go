4bcdbb9abe1c61ab3229235356b4487473ee517a
Mauro Mastrapasqua <mauro.mastrapasqua@studenti.unimi.it>
2020-01-23 17:09:40 +0100

    Scraper di car2go completo

---
+++ src/lib/scraper/car2go/car2go.go
@@ -0,0 +1,136 @@
+package car2go
+
+import (
+	"../../trip"
+	"../openstreetmap"
+	"../waze"
+
+	"encoding/json"
+	"fmt"
+	"math"
+	"io/ioutil"
+	"log"
+	// "strconv"
+)
+
+func GetTrips(from, to trip.Location, dirName string) []trip.Trip {
+	trips := make([]trip.Trip, 0)
+
+	carPosition := findTheClosestCar(from, dirName)
+	osmTrips := openstreetmap.GetTrips(from, carPosition)
+	wazeTrips := waze.GetTrips(carPosition, to)
+
+	// for i := 0; len(wazeTrips) == 0 && i < 100; i++ { // retry
+	// 	lat, _ := strconv.ParseFloat(carPosition.Latitude, 64)
+	// 	lon, _ := strconv.ParseFloat(carPosition.Longitude, 64)
+	// 	lat += float64(i) * 10e-6
+	// 	lon += float64(i) * 10e-6
+	//
+	// 	newCarPosition := trip.Location{fmt.Sprintf("%.06f", lat), fmt.Sprintf("%.06f"), "unknown"}
+	// 	wazeTrips = waze.GetTrips(newCarPosition, to)
+	// }
+
+	if len(osmTrips) > 1 && len(wazeTrips) > 0 {
+		car2goRoutes := Sum(osmTrips[1], wazeTrips[0])
+		trips = append(trips, car2goRoutes)
+		return trips
+	} else {
+		// Brutto
+		fmt.Println(from, carPosition)
+		fmt.Println(carPosition, to)
+
+		for _, trip := range osmTrips[1:] {
+			fmt.Println("\nERRORE")
+			fmt.Println(trip.TimeTable())
+			fmt.Println(trip.TimeTable())
+			fmt.Println("")
+		}
+
+		for _, trip := range wazeTrips {
+			fmt.Println("\nERRORE")
+			fmt.Println(trip.TimeTable())
+			fmt.Println(trip.TimeTable())
+			fmt.Println("")
+		}
+
+	}
+
+	return nil
+}
+
+func Sum(trip1, trip2 trip.Trip) trip.Trip {
+	var trip trip.Trip
+
+	trip.From = trip1.From
+	trip.To = trip2.To
+	trip.StartTime = trip1.StartTime
+	trip.EndTime = trip2.EndTime
+	trip.Duration = trip1.Duration + trip2.Duration
+	trip.Distance = trip1.Distance + trip2.Distance
+	trip.VehicleType = trip1.VehicleType + " + " + "SHARED CAR"
+	trip.ServiceName = "CAR2GO"
+	trip.ScrapedApp = trip1.ScrapedApp + " + " + trip2.ScrapedApp
+
+	return trip
+}
+
+func findTheClosestCar(from trip.Location, dirName string) trip.Location {
+	files, err := ioutil.ReadDir(dirName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// car2go_error.log is the last file,
+	// I need the previous one with len()-2
+	latestJsonDumpFilename := dirName + "/" + files[len(files) - 2].Name()
+
+	latestJsonDump, err := ioutil.ReadFile(latestJsonDumpFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	car2goResult := Car2goResult{}
+	err = json.Unmarshal([]byte(latestJsonDump), &car2goResult)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	closestCar := [2]float64{car2goResult[0].Loc[1], car2goResult[0].Loc[0]} // lon lat
+	minimumDistance := 100000000000.0
+	for _, result := range car2goResult[1:] {
+		// [lon, lat] inside json
+		if len(result.Loc) > 0 {
+			lat := result.Loc[1]
+			lon := result.Loc[0]
+
+			distance := distance(closestCar[0], closestCar[1], lat, lon)
+			if distance < minimumDistance {
+				minimumDistance = distance
+				closestCar[0] = lat
+				closestCar[1] = lon
+			}
+		}
+
+	}
+
+	closestCarPosition := trip.Location{
+		fmt.Sprintf("%.06f", closestCar[0]),
+		fmt.Sprintf("%.06f", closestCar[1]),
+		"unknown"}
+	return closestCarPosition
+}
+
+func distance(lat1, lon1, lat2, lon2 float64) float64 {
+	var la1, lo1, la2, lo2, earthRadius float64
+
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+	earthRadius = 6378100
+
+	h := hsin(la2-la1) + math.Cos(la1) * math.Cos(la2) * hsin(lo2-lo1)
+	return 2 * earthRadius * math.Asin(math.Sqrt(h))
+}
+
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
---
+++ src/lib/scraper/car2go/json.go
@@ -0,0 +1,23 @@
+package car2go
+
+type Car2goResult []struct {
+	Provider string    `json:"provider"`
+	Loc      []float64 `json:"loc"`
+	Obj      struct {
+		Manufacturer string `json:"manufacturer"`
+		Model        string `json:"model"`
+		Sign         string `json:"sign"`
+		Vin          string `json:"vin"`
+		FuelState    int    `json:"fuelState"`
+		EngineType   string `json:"engineType"`
+		Automatic    int    `json:"automatic"`
+		Color        string `json:"color"`
+		PriceInfo    struct {
+			Driving struct {
+				Formatted string `json:"formatted"`
+			} `json:"driving"`
+		} `json:"priceInfo"`
+		DeepLink string `json:"deepLink"`
+		Seats    int    `json:"seats"`
+	} `json:"obj"`
+}
