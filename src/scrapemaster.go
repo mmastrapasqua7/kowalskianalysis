04a9f45ac337a1efe4a1235b93b205a7014628d6
Mauro Mastrapasqua <mauro.mastrapasqua@studenti.unimi.it>
2020-02-02 11:37:58 +0100

    Compressione file json in .tar.xz, gestione errori, sha256checksum dei risultati

--- src/scrapemaster.go
+++ src/scrapemaster.go
@@ -10,11 +10,13 @@ import (
 	"./lib/trip"
 	"./lib/util"
 
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -25,9 +27,10 @@ type JsonRequestsFile []struct {
 }
 
 type ResultFile struct {
-	Id      int
-	Date    string
-	Results []Result
+	Id               int
+	Date             string
+	ResultsSha256Sum string
+	Results          []Result
 }
 
 type Result struct {
@@ -39,7 +42,7 @@ type Result struct {
 func main() {
 	// Arguments
 	if len(os.Args) < 4 {
-		fmt.Println("usage:\n\t scrapemaster [input.json] [scrapedDataDir] [outputDir]")
+		fmt.Println("usage:\n\t scrapemaster [richieste.json] [scraped_data_dir] [output_dir]")
 		os.Exit(0)
 	}
 	jsonRequestsFilename := os.Args[1]
@@ -47,16 +50,15 @@ func main() {
 	outputDir := os.Args[3]
 
 	// Logfile
-	logfile := createFile("scraper_error.log")
+	logfile := createFile(outputDir, "scraper_error.log")
 	log.SetOutput(logfile)
 	defer logfile.Close()
 
 	// Read requests
 	requests := readJsonRequests(jsonRequestsFilename)
-
 	for i := 0; true; i++ {
-		resultFilename := outputDir + "/scrapemaster_" + util.FormattedData(time.Now()) + ".json"
-		resultFile := createFile(resultFilename)
+		resultFilename := "scrapemaster_" + util.FormattedData(time.Now()) + ".json"
+		resultFile := createFile(outputDir, resultFilename)
 		var resultFileStruct ResultFile
 		resultFileStruct.Id = i
 		resultFileStruct.Date = time.Now().Format("2006-01-02 15:04:05")
@@ -67,39 +69,34 @@ func main() {
 			resultStruct.FromLon = request.From[1]
 			resultStruct.ToLat = request.To[0]
 			resultStruct.ToLon = request.To[1]
+			resultStruct.BigResult = getRoutesFromAllServices(
+				resultStruct.FromLat, resultStruct.FromLon,
+				resultStruct.ToLat, resultStruct.ToLon,
+				scrapedDataDir)
 
-			moovitResult := moovit.GetRoutes(request.From[0], request.From[1], request.To[0], request.To[1])
-			openstreetmapBikeResult := openstreetmap.GetBikeRoutes(request.From[0], request.From[1], request.To[0], request.To[1])
-			openstreetmapFootResult := openstreetmap.GetFootRoutes(request.From[0], request.From[1], request.To[0], request.To[1])
-			wazeResult := waze.GetRoutes(request.From[0], request.From[1], request.To[0], request.To[1])
-			car2goResult := car2go.GetRoutes(request.From[0], request.From[1], request.To[0], request.To[1], scrapedDataDir)
-			sharengoResult := sharengo.GetRoutes(request.From[0], request.From[1], request.To[0], request.To[1], scrapedDataDir)
-			enjoyResult := enjoy.GetRoutes(request.From[0], request.From[1], request.To[0], request.To[1], scrapedDataDir)
-
-			var results trip.BigJson
-			results.MoovitRoutes = moovitResult
-			results.OpenStreetMapBikeRoutes = openstreetmapBikeResult
-			results.OpenStreetMapFootRoutes = openstreetmapFootResult
-			results.WazeRoutes = wazeResult
-			results.Car2GoRoutes = car2goResult
-			results.EnjoyRoutes = enjoyResult
-			results.SharengoRoutes = sharengoResult
-
-			resultStruct.BigResult = results
 			resultFileStruct.Results = append(resultFileStruct.Results, resultStruct)
 
 			time.Sleep(30 * time.Second)
 		}
 
+		resultsData, _ := json.MarshalIndent(resultFileStruct.Results, "", "\t")
+		resultFileStruct.ResultsSha256Sum = fmt.Sprintf("%x", sha256.Sum256(resultsData))
+
 		data, _ := json.MarshalIndent(resultFileStruct, "", "\t")
-		_, err := resultFile.Write(data)
-		if err != nil {
-			log.Println("ERROR: scrapemaster: can't write to file" + resultFilename, err)
+		if _, err := resultFile.Write(data); err != nil {
+			log.Println("scrapemaster: can't write to file" + outputDir + "/" + resultFilename, err)
 		}
 
-		err = resultFile.Close()
-		if err != nil {
-			log.Println("ERROR: scrapemaster: can't close file" + resultFilename, err)
+		if err := resultFile.Close(); err != nil {
+			log.Println("scrapemaster: can't close file" + outputDir + "/" + resultFilename, err)
+		}
+
+		if err := compressFile(outputDir, resultFilename); err != nil {
+			log.Println("scrapemaster: can't compress file" + outputDir + "/" + resultFilename, err)
+		} else {
+			if err := os.Remove(outputDir + "/" + resultFilename); err != nil {
+				log.Println("scrapemaster: can't delete file" + outputDir + "/" + resultFilename, err)
+			}
 		}
 
 		// emp, _ := json.MarshalIndent(results, "", "  ")
@@ -108,29 +105,94 @@ func main() {
 	}
 }
 
+func getRoutesFromAllServices(fromLat, fromLon, toLat, toLon string, scrapedDataDir string) trip.BigJson {
+	moovitResult := moovit.GetRoutes(fromLat, fromLon, toLat, toLon)
+	openstreetmapBikeResult := openstreetmap.GetBikeRoutes(fromLat, fromLon, toLat, toLon)
+	openstreetmapFootResult := openstreetmap.GetFootRoutes(fromLat, fromLon, toLat, toLon)
+	wazeResult := waze.GetRoutes(fromLat, fromLon, toLat, toLon)
+	car2goResult := car2go.GetRoutes(fromLat, fromLon, toLat, toLon, scrapedDataDir)
+	sharengoResult := sharengo.GetRoutes(fromLat, fromLon, toLat, toLon, scrapedDataDir)
+	enjoyResult := enjoy.GetRoutes(fromLat, fromLon, toLat, toLon, scrapedDataDir)
+
+	var results trip.BigJson
+	results.MoovitRoutes = moovitResult
+	results.OpenStreetMapBikeRoutes = openstreetmapBikeResult
+	results.OpenStreetMapFootRoutes = openstreetmapFootResult
+	results.WazeRoutes = wazeResult
+	results.Car2GoRoutes = car2goResult
+	results.EnjoyRoutes = enjoyResult
+	results.SharengoRoutes = sharengoResult
+	return results
+}
+
+func compressFile(dir, filename string) error {
+	// pwd, cd
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if err := os.Chdir(dir); err != nil {
+		fmt.Println("scrapemaster: cd failed", err)
+		return err
+	}
+
+	// exec, wait
+	cmd := exec.Command("tar", "-cJf", filename + ".tar.xz", filename)
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+
+	// cd ..
+	if err := os.Chdir(currentDir); err != nil {
+		fmt.Println("scrapemaster: pwd failed", err)
+		return err
+	}
+	return nil
+}
+
 func readJsonRequests(jsonFilename string) JsonRequestsFile {
+	// read file
 	requestsData, err := ioutil.ReadFile(jsonFilename)
 	if err != nil {
-		log.Println("scrapemaster: can't open input file" + jsonFilename, err)
+		fmt.Println("scrapemaster: can't open input file " + jsonFilename + ":", err)
 		os.Exit(1)
 	}
 
-	requests := JsonRequestsFile{}
-	err = json.Unmarshal([]byte(requestsData), &requests)
-	if err != nil {
-		log.Println("scrapemaster: can't unmarshal json file" + jsonFilename, err)
+	// unmarshal
+	var requests JsonRequestsFile
+	if err := json.Unmarshal([]byte(requestsData), &requests); err != nil {
+		fmt.Println("scrapemaster: can't unmarshal json file " + jsonFilename + ":", err)
 		os.Exit(1)
 	}
-
 	return requests
 }
 
-func createFile(filename string) *os.File {
-	file, err := os.OpenFile(filename, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+func createFile(dir, filename string) *os.File {
+	// pwd, cd
+	currentDir, err := os.Getwd()
 	if err != nil {
-		log.Println("scrapemaster: can't open/create file" + filename, err)
+		fmt.Println("scrapemaster: pwd failed:", err)
+		os.Exit(1)
+	}
+	if err := os.Chdir(dir); err != nil {
+		fmt.Println("scrapemaster: chdir failed:", err)
 		os.Exit(1)
 	}
 
+	// open append / create append
+	file, err := os.OpenFile(filename, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("scrapemaster: can't open/create file" + dir + "/" + filename + ":", err)
+		os.Exit(1)
+	}
+
+	// cd ..
+	if err := os.Chdir(currentDir); err != nil {
+		fmt.Println("scrapemaster: chdir failed:", err)
+		os.Exit(1)
+	}
 	return file
 }
