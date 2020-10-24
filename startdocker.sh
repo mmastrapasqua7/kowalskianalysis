a9d3ccd3c1fd9eb61b70d276679a7cd328bcfd2e
Mauro Mastrapasqua <mauro.mastrapasqua@studenti.unimi.it>
2020-10-24 18:06:29 +0200

    Fine delle analisi post lockdown

--- startdocker.sh
+++ startdocker.sh
@@ -1,1 +1,1 @@
-docker run --rm -ti -p 127.0.0.1:8888:8888 -v $(pwd)/jupyter:/home/jovyan/tesi dariomalchiodi/sad
+sudo systemctl start docker; docker run --rm -ti -p 127.0.0.1:8888:8888 -v $(pwd)/jupyter:/home/jovyan/tesi dariomalchiodi/sad
